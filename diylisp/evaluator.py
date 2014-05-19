5e957fce08c5f26757768d7a7b60743d9dd4ba6e
Jorren Bosga <jorren.bosga@hotmail.com>
2014-05-20 00:42:55 +0200

    Part 5 passes first 11 tests.

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -35,15 +35,32 @@ def evaluate(ast, env):
         return ast[1]
     if ast[0] == 'eq':
         return is_atom(evaluate(ast[1], env)) and is_atom(evaluate(ast[2], env)) and evaluate(ast[1], env) == evaluate(ast[2], env)
+
     if ast[0] == 'define':
         assert_valid_definition(ast[1:])
         symbol = ast[1]
         value = evaluate(ast[2], env)
         env.set(symbol, value)
         return symbol
+
     if ast[0] == 'lambda':
         if len(ast) != 3:
             raise LispError("Wrong number of arguments")
+        if not is_list(ast[1]):
+            raise LispError("Parameters are not in list-form")
+        else:
+            return Closure(env, ast[1], ast[2])
+
+    if is_closure(ast[0]):
+        closure = ast[0]
+        arguments = ast[1:]
+        if len(arguments) != len(closure.params):
+           errormessage = "wrong number of arguments, expected", len(closure.params), "got", len(arguments)
+           raise LispError(errormessage)
+        arguments = [evaluate(a, env) for a in arguments]
+        bindings = dict(zip(closure.params, arguments))
+        new_env = closure.env.extend(bindings)
+        return evaluate(closure.body, new_env)
 
 
 
